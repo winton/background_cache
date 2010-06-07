31b04b3fabf6ff95d3bd5e832479dfb55179709d
winton <mail@wintoni.us>
2010-06-07 13:54:42 -0700

    Moving boot logic to its own method, manual caching now works properly

--- lib/background_cache.rb
+++ lib/background_cache.rb
@@ -2,6 +2,7 @@ require File.expand_path("#{File.dirname(__FILE__)}/../require")
 Require.lib!
 
 module BackgroundCache
+  
   class AppInstance
     include ActionController::UrlWriter
     include Rack::Test::Methods
@@ -9,31 +10,35 @@ module BackgroundCache
       ActionController::Dispatcher.new
     end
   end
+  
   def self.cache!(group=nil)
-    key = set_key!
-    # Used to make requests
-    instance = AppInstance.new
-    # Load the application background cache config (stay dynamic)
-    instance.get("/?background_cache_load=#{key}")
-    # Retrieve caches from config
-    load RAILS_ROOT + "/lib/background_cache_config.rb"
+    key, instance = boot
     caches = BackgroundCache::Config.caches
     caches.each do |cache|
       next if group && cache[:group] != group
-      # Get URL
       url = cache[:path] || instance.url_for(cache[:params].merge(:only_path => true))
       puts "(#{cache[:group]}) #{url}"
-      # Request action with ?background_cache
       instance.get(url + "#{url.include?('?') ? '&' : '?'}background_cache=#{key}")
     end
   end
+  
   def self.manual(url)
-    key = set_key!
-    instance = AppInstance.new
+    key, instance = boot
     url = instance.url_for(url.merge(:only_path => true)) if url.respond_to?(:keys)
     instance.get(url + "#{url.include?('?') ? '&' : '?'}background_cache=#{key}")
     url
   end
+  
+  private
+  
+  def self.boot
+    key = set_key!
+    instance = AppInstance.new
+    instance.get("/?background_cache_load=#{key}")
+    load RAILS_ROOT + "/lib/background_cache_config.rb"
+    [ key, instance ]
+  end
+  
   def self.set_key!
     key = BackgroundCache::Config.key
     ::ActionController::Base.cache_store.write('background_cache/key', key)
